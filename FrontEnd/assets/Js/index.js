f8e6cbdf1e80b51e8b44990445d4891f047a3908
Rakotoson theo <Rakotoson.theo@gmail.com>
2023-06-03 18:44:31 +0200

    affichage dynamique de la gallerie et des catégories

--- FrontEnd/assets/Js/index.js
+++ FrontEnd/assets/Js/index.js
@@ -1,22 +1,63 @@
-const apiUrl = "http://localhost:5678/api/works"
+const worksUrl = "http://localhost:5678/api/works";
+const categorieUrl = "http://localhost:5678/api/categories";
 
-fetch("http://localhost:5678/api/works")
-.then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
+
+const gallery = document.querySelector(".gallery")
+const categories_container = document.querySelector(".categories_container")
+
+
+
+fetch(worksUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Le tableau de données se trouve dans la variable 'data'
+    console.table(data);
+    // Vous pouvez accéder aux éléments du tableau comme ceci :
+    data.forEach(element => {
+      // console.log(element.title);
+      // console.log(element.imageUrl);
+
+      image_card(element);
+
+      function image_card(element){
+        gallery.innerHTML += `
+        <figure>
+				  <img src="${element.imageUrl}" alt="${element.title}">
+				  <figcaption>${element.title}</figcaption>
+			  </figure>
+        `
+      };
+    });
+
   })
-  .then(function(value) {
-    console.log(value);
-    
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-    console.log(err);
+  .catch(error => {
+    console.log("erreur");
   });
 
+ fetch(categorieUrl)
+  .then(res => res.json())
+  .then(data =>{
 
+    // console.log(data);
 
+    data.forEach(element => {
+      // console.log(element.name);
+      // console.log(element.id);
 
+      categorie(element);
 
+      function categorie(element){
+        categories_container.innerHTML += `
+        <button class="projet_button" id="${element.id}">${element.name}</button>
+        `      
+      };
+    });
+  });
 
+let button = document.querySelector(".projet_button")
+
+  
+function activeCategorie(){
+  
+};
+ 
