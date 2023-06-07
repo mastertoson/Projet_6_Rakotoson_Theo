f716509e8379b8bd7583bee4b3ba5c27a392c17f
Rakotoson theo <Rakotoson.theo@gmail.com>
2023-06-08 01:39:55 +0200

    ajout et finalisation page login ajout modale

--- FrontEnd/assets/Js/index.js
+++ FrontEnd/assets/Js/index.js
@@ -1,63 +1,229 @@
 const worksUrl = "http://localhost:5678/api/works";
 const categorieUrl = "http://localhost:5678/api/categories";
 
-
 const gallery = document.querySelector(".gallery")
-const categories_container = document.querySelector(".categories_container")
+let categories_container = document.querySelector(".categories_container")
+let buttonAll = document.querySelector('.all');
+const modaleImgContainer = document.querySelector('.modal_img_container');
 
+async function init() {
 
-
-fetch(worksUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Le tableau de données se trouve dans la variable 'data'
-    console.table(data);
-    // Vous pouvez accéder aux éléments du tableau comme ceci :
-    data.forEach(element => {
-      // console.log(element.title);
-      // console.log(element.imageUrl);
-
-      image_card(element);
-
-      function image_card(element){
-        gallery.innerHTML += `
-        <figure>
-				  <img src="${element.imageUrl}" alt="${element.title}">
-				  <figcaption>${element.title}</figcaption>
-			  </figure>
-        `
-      };
-    });
-
-  })
-  .catch(error => {
-    console.log("erreur");
-  });
-
- fetch(categorieUrl)
-  .then(res => res.json())
-  .then(data =>{
-
-    // console.log(data);
-
-    data.forEach(element => {
-      // console.log(element.name);
-      // console.log(element.id);
-
-      categorie(element);
-
-      function categorie(element){
-        categories_container.innerHTML += `
-        <button class="projet_button" id="${element.id}">${element.name}</button>
-        `      
-      };
-    });
-  });
-
-let button = document.querySelector(".projet_button")
-
+  let allinfo = await fetchWorks();
+  console.log(allinfo);
+  displayinfo(allinfo);
   
-function activeCategorie(){
+  let allCategory = await fetchCategory();
+  displayCategory(allCategory);
+  filtreCat();
+
+}
+
+init();
+
+//localstorage
+
+const logInStorage = localStorage.getItem('log')?.toString();
+let logIn = ["LocalStorage vide"];
+if (logInStorage) {
+  logIn = JSON.parse(logInStorage);
+};
+
+
+let loginButton = document.querySelector('.login_container');
+let editionContainer = document.querySelector('.edition_container')
+let ProjetContainer = document.querySelector('.projet_container')
+let introModify = document.querySelector('.intro_modify')
+const modify = `<div class="modify">
+<i class="fa-solid fa-pen-to-square modify_icon"></i>
+<a class="modify_text open_modale" href="#modale">modifier</a>
+</div>`
+
+
+
+
+if (logIn == "LocalStorage vide") {
+  console.log("vide");
+  loginButton.innerHTML = `<a href="login.html" class="login_button">login</a>`;
+
+
+} else {
+  loginButton.innerHTML = `<a href="#" class="login_button logout">logout</a>`;
+  editionContainer.classList.add(`display_on`);
+  categories_container.classList.add(`display_off`);
+  introModify.classList.add(`display_on`);
+  ProjetContainer.classList.add(`padding_bottom`);
+  ProjetContainer.insertAdjacentHTML("beforeend", `${modify}`)
   
 };
- 
+
+let logoutButton = document.querySelector('.logout');
+logoutButton.addEventListener('click', async (e) => {
+  localStorage.clear();
+  location.reload();
+});
+
+
+
+function fetchWorks() {
+  return fetch(worksUrl)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+};
+
+function fetchCategory() {
+  return fetch(categorieUrl)
+    .then((res) => {
+      return res.json()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function displayinfo(allinfo) {
+  gallery.innerHTML = "";
+  for (const info of allinfo) {
+    
+    gallery.insertAdjacentHTML("beforeend", `
+        <figure>
+				  <img src="${info.imageUrl}" alt="${info.title}">
+				  
+			  </figure>
+        `
+    )
+    modaleImgContainer.insertAdjacentHTML("beforeend",`
+    
+    <div class="img_img_container" data-id="${info.id}">
+          <div class="icon_container">
+           <i class="fa-solid fa-arrows-up-down-left-right icon"></i> 
+					 <i class="fa-regular fa-trash-can icon delete"></i>
+          </div>
+          
+          <img class="modal_img" src="${info.imageUrl}" alt="${info.title}">
+					<P>éditer</P>
+					
+				</div>`)
+        
+        
+        
+  };
+}
+
+function displayCategory(allinfo) {
+
+  for (const info of allinfo) {
+    categories_container.insertAdjacentHTML("beforeend",
+      `
+            <button class="projet_button " id="${info.id}" >${info.name}</button>
+            `
+    );
+  };
+
+};
+
+
+function filtreCat() {
+
+  buttonAll.addEventListener("click", async (e) => {
+
+    let allinfo = await fetchWorks();
+
+    displayinfo(allinfo);
+
+  });
+  let button = document.querySelectorAll(".projet_button");
+  for (const cat of button) {
+    cat.addEventListener("click", async (e) => {
+      let allinfo = await fetchWorks();
+      const filtreInfo = allinfo.filter(info => info.categoryId == cat.id);
+      displayinfo(filtreInfo);
+    })
+  };
+};
+
+//modale
+let modal = null
+
+const openModal = function (e){
+  e.preventDefault()
+
+  const target = document.querySelector(e.target.getAttribute('href'))
+  target.style.display = null
+  target.removeAttribute('aria-hidden')
+  target.setAttribute('aria-modal', 'true')
+  modal = target
+  modal.addEventListener('click', closeModal)
+  modal.querySelector('.close_modal').addEventListener('click', closeModal)
+  modal.querySelector('.modale-wrapper').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+  if(modal == null) return
+  e.preventDefault()
+  modal.style.display = "none"
+  modal.setAttribute('aria-hidden', 'true')
+  modal.removeAttribute('aria-modal')
+  modal.removeEventListener('click', closeModal)
+  modal.querySelector('.close_modal').removeEventListener('click', closeModal)
+  modal.querySelector('.modale-wrapper').removeEventListener('click', stopPropagation)
+  
+  modal = null
+}
+
+document.querySelectorAll('.open_modale').forEach(a => {
+  a.addEventListener('click', openModal)
+  
+})
+
+const stopPropagation = function (e){
+  e.stopPropagation()
+}
+//endmodale
+
+
+
+
+modaleImgContainer.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete')) {
+    event.preventDefault(); // Empêcher le rechargement de la page
+
+    const elementToDelete = event.target.closest('.img_img_container');
+    if (elementToDelete) {
+      const infoId = elementToDelete.dataset.id;
+      const supprUrl = `http://localhost:5678/api/works/${infoId}`;
+
+      fetch(supprUrl, {
+        method: "DELETE",
+        headers: {
+          "Accept": "*/*",
+          "Authorization": 'Bearer ' + logIn.token
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Supprimé');
+          elementToDelete.remove();
+        } else {
+          console.error('Erreur lors de la suppression.');
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la suppression:', error);
+      });
+    }
+  } else {
+    console.log("ff");
+  }
+});
+
+
+
+
+
+
+
+
---
+++ FrontEnd/assets/Js/log.js
@@ -0,0 +1,55 @@
+const log = "http://localhost:5678/api/users/login";
+
+
+
+
+
+
+
+let submit = document.getElementById("submit")
+const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
+const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/;
+
+submit.addEventListener("click", (event) => {
+    // préparation les infos pour l'envoie en POST
+    let contact = {
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value
+    };
+    // validation que le formulaire soit correctement rempli
+    console.log(contact);
+    
+    if (
+      (regexMail.test(contact.email) == true) &
+      (regexPassword.test(contact.password) == true)
+    ) {
+      event.preventDefault();
+      
+      // envoie en POST
+      fetch(log, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json", 
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( contact ),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          
+          localStorage.setItem('log', JSON.stringify(data));
+          window.location.href = "index.html"
+        })
+        .catch((erreur) => alert(erreur));
+    } else {
+      alert(
+        "Veuillez correctement renseigner l'entièreté du formulaire pour valider votre connexion."
+      );
+    }
+  });
+
+
+
+
+
